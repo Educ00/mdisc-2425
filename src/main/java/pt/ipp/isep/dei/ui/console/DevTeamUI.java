a359fe4a4ebad3afc9b9e44255e2bc6376908aac
Eduardo Carreiro <109857329+Educ00@users.noreply.github.com>
2025-04-16 10:02:48 +0100

    Added name to Dev Team Page

--- src/main/java/pt/ipp/isep/dei/ui/console/DevTeamUI.java
+++ src/main/java/pt/ipp/isep/dei/ui/console/DevTeamUI.java
@@ -7,7 +7,7 @@ public class DevTeamUI implements Runnable {
     public void run() {
         System.out.println("\n");
         System.out.println("--- DEVELOPMENT TEAM -------------------");
-        System.out.println("  Student Name 1 - 1XXXXXX@isep.ipp.pt");
+        System.out.println("  Eduardo Carreiro - 1211199@isep.ipp.pt");
         System.out.println("  Student Name 2 - 1XXXXXX@isep.ipp.pt");
         System.out.println("  Student Name 3 - 1XXXXXX@isep.ipp.pt");
         System.out.println("  Student Name 4 - 1XXXXXX@isep.ipp.pt");
