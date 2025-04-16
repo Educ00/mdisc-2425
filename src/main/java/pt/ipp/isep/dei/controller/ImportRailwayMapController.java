f0c0f9ec23273cf005fa880c43fdb7c7477a5cd1
Eduardo Carreiro <109857329+Educ00@users.noreply.github.com>
2025-04-16 13:29:08 +0100

    Logic refactored to controller

--- src/main/java/pt/ipp/isep/dei/controller/ImportRailwayMapController.java
+++ src/main/java/pt/ipp/isep/dei/controller/ImportRailwayMapController.java
@@ -1,7 +1,13 @@
 package pt.ipp.isep.dei.controller;
 
+import pt.ipp.isep.dei.domain.Connection;
+import pt.ipp.isep.dei.domain.Station;
 import pt.ipp.isep.dei.repository.RailwayRepository;
 import pt.ipp.isep.dei.repository.Repositories;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class ImportRailwayMapController {
 
@@ -19,6 +25,38 @@ public class ImportRailwayMapController {
     }
 
     public boolean importFile(String filepath) {
-        return this.railwayRepository.importFile(filepath);
+        Set<Station> stationSet = new HashSet<>();
+        Station station1 = new Station("Station1", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0 );
+        Station station2 = new Station("Station2", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0 );
+        Station station3 = new Station("Station3", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0 );
+        Station station4 = new Station("Station4", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0 );
+        Station station5 = new Station("Station5", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0 );
+        stationSet.add(station1);
+        stationSet.add(station2);
+        stationSet.add(station3);
+        stationSet.add(station4);
+        stationSet.add(station5);
+        for (Station station : stationSet) {
+            this.railwayRepository.addNode(station);
+        }
+
+        Set<Connection> connectionSet = new HashSet<>();
+        Connection connection1 = new Connection(1, station1, station2);
+        Connection connection2 = new Connection(1, station2, station3);
+        Connection connection3 = new Connection(1, station3, station4);
+        Connection connection4 = new Connection(1, station4, station5);
+        Connection connection5 = new Connection(1, station2, station5);
+        Connection connection6 = new Connection(1, station5, station2);
+        connectionSet.add(connection1);
+        connectionSet.add(connection2);
+        connectionSet.add(connection3);
+        connectionSet.add(connection4);
+        connectionSet.add(connection5);
+        connectionSet.add(connection6);
+        for (Connection connection : connectionSet) {
+            this.railwayRepository.addEdge(connection);
+        }
+
+        return true;
     }
 }
--- src/main/java/pt/ipp/isep/dei/repository/RailwayRepository.java
+++ src/main/java/pt/ipp/isep/dei/repository/RailwayRepository.java
@@ -19,39 +19,6 @@ public class RailwayRepository {
         this.railway = new SingleGraph("Railway");
     }
 
-    public boolean importFile(String filepath) {
-        // TODO: Implementar a logica para importar do ficheiro
-        // para já, criamos um grafo de teste atoa.
-        Station station1 = new Station("Station1", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0 );
-        Station station2 = new Station("Station2", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0 );
-        Station station3 = new Station("Station3", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0 );
-        Station station4 = new Station("Station4", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0 );
-        Station station5 = new Station("Station5", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0 );
-
-        Node node1 = addNode(station1);
-        Node node2 = addNode(station2);
-        Node node3 = addNode(station3);
-        Node node4 = addNode(station4);
-        Node node5 = addNode(station5);
-
-
-        Connection connection1 = new Connection(1, station1, station2);
-        Connection connection2 = new Connection(1, station2, station3);
-        Connection connection3 = new Connection(1, station3, station4);
-        Connection connection4 = new Connection(1, station4, station5);
-        Connection connection5 = new Connection(1, station2, station5);
-        Connection connection6 = new Connection(1, station5, station2);
-
-        Edge edge1 = addEdge(connection1);
-        Edge edge2 = addEdge(connection2);
-        Edge edge3 = addEdge(connection3);
-        Edge edge5 = addEdge(connection4);
-        Edge edge6 = addEdge(connection5);
-        Edge edge7 = addEdge(connection6);
-
-        return true;
-    }
-
     public void displayGraph(Graph customGraph) {
         customGraph.setAttribute("ui.quality");
         customGraph.setAttribute("ui.antialias");
